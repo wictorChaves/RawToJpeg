221fd9b366ef1cc4182f0859de4aa9d2f21a07c1
Wictor Chaves <wictor@hotmail.com.br>
2020-02-09 19:47:20 -0300

    Done

--- RawToJpeg/Program.cs
+++ RawToJpeg/Program.cs
@@ -4,17 +4,60 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
 
 namespace RawToJpeg
 {
     class Program
     {
+
+        [STAThread]
         static void Main(string[] args)
         {
-            using (MagickImage image = new MagickImage("C:\\Users\\wictor\\Downloads\\IMG_2581.CR2"))
+            var origem = "";
+            var destino = "";
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                origem = fbd.SelectedPath;
+            else
+                Environment.Exit(0);
+
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+                destino = fbd.SelectedPath;
+            else
+                Environment.Exit(0);
+
+
+            Console.Write("Processando... ");
+            foreach (var filePath in Directory.GetFiles(origem))
             {
-                image.Write("C:\\Users\\wictor\\Downloads\\IMG_2581.jpg");
+                var arrayPath = filePath.Split('\\');
+                var fileName = arrayPath[arrayPath.Length - 1];
+
+                var arrayFilename = fileName.Split('.');
+                var ext = arrayFilename[arrayFilename.Length - 1];
+                var name = arrayFilename[0];
+                var target = destino + "\\" + name + ".jpg";
+
+                RawToJpeg(filePath, target);
+            }
+            Console.Write("Fim do processo");
+            Console.ReadKey();
+        }
+
+        public static void RawToJpeg(string source, string target)
+        {
+            using (MagickImage image = new MagickImage(source))
+            {
+                image.Write(target);
             }
         }
+
     }
 }
